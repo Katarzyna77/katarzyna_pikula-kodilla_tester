e0f7d38ba83c33f3052901335c6f59b8f5eb90cb
Katarzyna Pikula <kaskapi77@gmail.com>
2019-12-29 15:34:47 +0100

    Loops

---
+++ kodilla-intro/src/main/java/loops/Loops.java
@@ -0,0 +1,25 @@
+package loops;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int total = 0;
+
+        for (int i = 0; i <= numbers.length; i++) {
+            total = total + i;
+        }
+        System.out.println(total);
+
+        int product = 1;
+        for (int i = 1; i <= numbers.length; i++) {
+            product = product * i;
+        }
+        System.out.println(product);
+    }
+}
